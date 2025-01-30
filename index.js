a66faa92d171fc0323a6c0bc5d261b943a1662b1
Emmanuel Muogbo <86425681+Nuel-MD@users.noreply.github.com>
2025-01-30 02:43:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000; // Port for local development
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
@@ -10,13 +10,13 @@ app.use(cors());
 app.get('/', (req, res) => {
     const response = {
         email: 'eimuogbo@gmail.com',  
-        current_datetime: new Date().toISOString(), // Current date in ISO 8601 format
-        github_url: 'https://github.com/Nuel-MD/your-repo',  // Replace with your GitHub URL
+        current_datetime: new Date().toISOString(), 
+        github_url: 'https://github.com/Nuel-MD/hng-public-api',  
     };
     res.status(200).json(response);
 });
 
-// Start the server
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
