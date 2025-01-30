fb7ef6449e41929c1bce8f9f192a162287b86e7c
Emmanuel Muogbo <muogboemmanuel9@example.com>
2025-01-30 20:42:10 +0100

    Update index.js and .gitignore

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 const express = require('express');
 const cors = require('cors');
+const fetch = require('node-fetch'); 
 const app = express();
 const port = process.env.PORT || 3000;
+
+require('dotenv').config();
 
 // Middleware
 app.use(cors());
 
 // Route to handle the GET request
 app.get('/', (req, res) => {
-    const response = {
-        email: 'eimuogbo@gmail.com',  
-        current_datetime: new Date().toISOString(), 
-        github_url: 'https://github.com/Nuel-MD/hng-public-api',  
-    };
-    res.status(200).json(response);
+  fetch('https://example.com/protected-url', {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${process.env.BEARER_TOKEN}`
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      res.status(200).json({
+        email: 'eimuogbo@gmail.com',
+        current_datetime: new Date().toISOString(),
+        github_url: 'https://github.com/Nuel-MD/hng-public-api',
+        external_data: data, 
+      });
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      res.status(500).json({ error: 'Failed to fetch data from external URL' });
+    });
 });
 
-
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
